490f40714cbf728d67d8e3833cbf714765a47672
Michael Price <price@kilentra.net>
2018-02-25 16:13:59 -0500

    Remove duplicative aud_slots_out_read interfaces in da_platform
    These were originally put in to separately buffer each slot's outgoing
    audio samples on their way to the host.  But it turns out this isn't
    necessary since we can only read a chunk of samples from one slot
    at a time.  Let the FIFO arbiter do this job for us.
    
    Also, this was causing bus contention on the FIFO interface "ready"
    signals going into the FIFO arbiter.

--- verilog/da_platform.sv
+++ verilog/da_platform.sv
@@ -207,13 +207,16 @@ assign hwcon_parallel[7:4] = 4'b0000;
       "Out" refers to data going to the host (i.e. responses, or ADC samples).
     - For audio, "Write" refers to the FIFO going into the arbiter, "read" is the one coming out.
       The connection of these FIFOs is reversed for the "in" and "out" directions.
+      
+      Note that we don't need aud_slots_out_read since the data can only be read
+      into one FIFO at a time, and that is audio_out_fifo (defined below, after
+      the generate loop for per-slot logic).
  */
 
 FIFOInterface #(.num_bits(32)) aud_slots_in_write[num_slots] (clk_core);
 FIFOInterface #(.num_bits(32)) aud_slots_in_read[num_slots] (clk_core);
 
 FIFOInterface #(.num_bits(32)) aud_slots_out_write[num_slots] (clk_core);
-FIFOInterface #(.num_bits(32)) aud_slots_out_read[num_slots] (clk_core);
 
 (* keep = "true" *) FIFOInterface #(.num_bits(host_width)) ctl_slots_in[num_slots] (clk_core);
 (* keep = "true" *) FIFOInterface #(.num_bits(host_width)) ctl_slots_out[num_slots] (clk_core);
@@ -245,9 +248,6 @@ generate for (g = 0; g < num_slots; g++) always_comb begin
     aud_slots_out_write[g].ready = arb_in_ready[num_slots + g];
     arb_in_enable[num_slots + g] = aud_slots_out_write[g].valid;
     arb_in_data[num_slots + g] = aud_slots_out_write[g].data;
-    arb_out_ready[num_slots + g] = aud_slots_out_read[g].ready;
-    aud_slots_out_read[g].valid = arb_out_enable[num_slots + g];
-    aud_slots_out_read[g].data = arb_out_data[num_slots + g];
 end
 endgenerate
 
@@ -361,12 +361,10 @@ generate for (g = 0; g < num_slots; g++) begin: slots
         if (slot_index == g) begin
             aud_slots_in_write[g].valid = aud_in_write.valid;
             aud_slots_in_write[g].data = aud_in_write.data;
-            aud_slots_out_read[g].ready = aud_out_read.ready;
         end
         else begin
             aud_slots_in_write[g].valid = 0;
             aud_slots_in_write[g].data = 0;
-            aud_slots_out_read[g].ready = 0;
         end
     end
     
