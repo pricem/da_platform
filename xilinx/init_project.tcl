2911b84f316e5bbc83619fdcef4b422a1cb0966d
Michael Price <price@kilentra.net>
2018-04-01 21:12:35 -0400

    Update Xilinx project script for MIG simulation options
    We now create two simulation sets, sim_real_mig and sim_fake_mig.
    As you might imagine this lets you choose (via current_fileset or
    "Make Active") whether to use the full MIG design and DDR3 memory
    model, or just a behavioral model of what an AXI slave memory
    should do.  The simulation can take a while when you use the full
    MIG.

--- xilinx/init_project.tcl
+++ xilinx/init_project.tcl
@@ -113,57 +113,170 @@ set_property -name "file_type" -value "XDC" -objects $file_obj
 set obj [get_filesets constrs_1]
 set_property -name "target_constrs_file" -value "[file normalize "constraints/debug.xdc"]" -objects $obj
 
-# Create 'sim_1' fileset (if not found)
-if {[string equal [get_filesets -quiet sim_1] ""]} {
-  create_fileset -simset sim_1
+#   Create two main simulation filesets:
+#   a) sim_fake_mig - Simulate a behavioral model of AXI slave memory, bypassing the MIG and DDR3 model.
+#   b) sim_real_mig - Simulate with full MIG and DDR3 memory RTL models.
+create_fileset -simset sim_fake_mig
+create_fileset -simset sim_real_mig
+current_fileset -simset [get_filesets sim_fake_mig]
+delete_fileset sim_1
+
+set base_sim_files [list \
+    "[file normalize "../verilog/clk_divider.sv"]" \
+    "[file normalize "../verilog/delay.sv"]" \
+    "[file normalize "../verilog/deserializer.sv"]" \
+    "[file normalize "../verilog/contrib/ezusb_io.v"]" \
+    "[file normalize "../verilog/fifo_async.sv"]" \
+    "[file normalize "../verilog/fifo_sync.sv"]" \
+    "[file normalize "../verilog/serializer.sv"]" \
+    "[file normalize "../verilog/commands.vh"]" \
+    "[file normalize "../verilog/slot_controller.sv"]" \
+    "[file normalize "../verilog/spi_master.sv"]" \
+    "[file normalize "../verilog/spi_slave.sv"]" \
+    "[file normalize "../verilog/da_platform.sv"]" \
+    "[file normalize "../verilog/da_platform_wrapper.sv"]" \
+    "[file normalize "../verilog/structures.sv"]" \
+    "[file normalize "../verilog/fifo_arbiter.sv"]" \
+    "[file normalize "../verilog/fx2_model.sv"]" \
+    "[file normalize "../verilog/i2s_receiver.sv"]" \
+    "[file normalize "../verilog/i2s_source.sv"]" \
+    "[file normalize "../verilog/interfaces.sv"]" \
+    "[file normalize "../verilog/isolator_model.sv"]" \
+    "[file normalize "../verilog/mig_adapter.sv"]" \
+    "[file normalize "../verilog/slot_model.sv"]" \
+    "[file normalize "../verilog/da_platform_tb.sv"]" \
+]
+add_files -norecurse -fileset sim_fake_mig $base_sim_files
+add_files -norecurse -fileset sim_real_mig $base_sim_files
+
+set fake_mig_sim_files [list \
+    "[file normalize "../verilog/mem_model_axi.sv"]" \
+]
+add_files -norecurse -fileset sim_fake_mig $fake_mig_sim_files
+
+set real_mig_sim_files [list \
+    "[file normalize "../verilog/contrib/ddr3_model/ddr3_model.sv"]"\
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_mc_cmd_translator.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_mc_wr_cmd_fsm.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_ctrl_reg.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_ctrl_read.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_ddr_axi_upsizer.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_mc_b_channel.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_ddr_a_upsizer.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_ddr_axic_register_slice.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_ddr_carry_latch_and.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_mc_cmd_arbiter.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_ctrl_write.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_ddr_carry_latch_or.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_mc_ar_channel.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_mc_simple_fifo.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_ddr_comparator.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_ddr_command_fifo.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_mc_fifo.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_mc_r_channel.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_mc_incr_cmd.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_mc_cmd_fsm.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_ctrl_top.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_mc.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_mc_w_channel.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_ctrl_addr_decode.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_ddr_axi_register_slice.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_mc_wrap_cmd.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_ddr_comparator_sel_static.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_ddr_comparator_sel.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_ddr_carry_and.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_ctrl_reg_bank.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_axi_mc_aw_channel.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_ddr_w_upsizer.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_ddr_r_upsizer.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/axi/mig_7series_v4_0_ddr_carry_or.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/mig_7series_0.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/clocking/mig_7series_v4_0_tempmon.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/clocking/mig_7series_v4_0_infrastructure.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/clocking/mig_7series_v4_0_iodelay_ctrl.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/clocking/mig_7series_v4_0_clk_ibuf.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/ui/mig_7series_v4_0_ui_rd_data.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/ui/mig_7series_v4_0_ui_cmd.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/ui/mig_7series_v4_0_ui_top.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/ui/mig_7series_v4_0_ui_wr_data.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/controller/mig_7series_v4_0_bank_queue.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/controller/mig_7series_v4_0_bank_state.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/controller/mig_7series_v4_0_rank_mach.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/controller/mig_7series_v4_0_round_robin_arb.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/controller/mig_7series_v4_0_bank_mach.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/controller/mig_7series_v4_0_arb_mux.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/controller/mig_7series_v4_0_rank_common.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/controller/mig_7series_v4_0_bank_compare.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/controller/mig_7series_v4_0_bank_cntrl.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/controller/mig_7series_v4_0_arb_select.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/controller/mig_7series_v4_0_arb_row_col.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/controller/mig_7series_v4_0_col_mach.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/controller/mig_7series_v4_0_bank_common.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/controller/mig_7series_v4_0_mc.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/controller/mig_7series_v4_0_rank_cntrl.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_top.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_prbs_gen.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_init.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_of_pre_fifo.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_dqs_found_cal.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_mc_phy_wrapper.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_if_post_fifo.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_mc_phy.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_poc_top.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_ck_addr_cmd_delay.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_byte_lane.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_wrlvl.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_wrcal.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_oclkdelay_cal.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_ocd_mux.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_poc_meta.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_ocd_lim.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_poc_pd.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_byte_group_io.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_ocd_edge.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_ocd_cntlr.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_ocd_samp.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_wrlvl_off_delay.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_poc_edge_store.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_poc_tap_base.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_dqs_found_cal_hr.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_calib_top.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_ocd_po_cntlr.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_prbs_rdlvl.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_skip_calib_tap.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_4lanes.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_poc_cc.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_tempmon.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_ocd_data.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/phy/mig_7series_v4_0_ddr_phy_rdlvl.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/mig_7series_0_mig_sim.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/ip_top/mig_7series_v4_0_mem_intfc.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/ip_top/mig_7series_v4_0_memc_ui_top_axi.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/ip_top/mig_7series_v4_0_memc_ui_top_std.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/ecc/mig_7series_v4_0_ecc_dec_fix.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/ecc/mig_7series_v4_0_ecc_merge_enc.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/ecc/mig_7series_v4_0_fi_xor.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/ecc/mig_7series_v4_0_ecc_gen.v"]" \
+    "[file normalize "./ip/mig_7series_0/user_design/rtl/ecc/mig_7series_v4_0_ecc_buf.v"]" \
+]
+add_files -norecurse -fileset sim_real_mig $real_mig_sim_files
+
+foreach fset [get_filesets sim*] {
+    puts "Preparing configuration for fileset $fset"
+
+    add_files -fileset $fset [file normalize "./da_platform_tb_behav.wcfg"]
+
+    set_property -name "file_type" -value "SystemVerilog" -objects [get_files -of_objects $fset [list "*.sv"]]
+    set_property -name "file_type" -value "Verilog Header" -objects [get_files -of_objects $fset [list "*.vh"]]
+    
+    set_property -name "nl.sdf_anno" -value "0" -objects $fset
+    set_property -name "source_set" -value "" -objects $fset
+    set_property -name "top" -value "da_platform_tb" -objects $fset
+    set_property -name "xsim.simulate.runtime" -value "3000us" -objects $fset
 }
 
-# Set 'sim_1' fileset object
-set obj [get_filesets sim_1]
-set files [list \
- "[file normalize "../verilog/clk_divider.sv"]"\
- "[file normalize "../verilog/delay.sv"]"\
- "[file normalize "../verilog/deserializer.sv"]"\
- "[file normalize "../verilog/contrib/ezusb_io.v"]"\
- "[file normalize "../verilog/fifo_async.sv"]"\
- "[file normalize "../verilog/fifo_sync.sv"]"\
- "[file normalize "../verilog/mem_model_axi.sv"]"\
- "[file normalize "../verilog/serializer.sv"]"\
- "[file normalize "../verilog/commands.vh"]"\
- "[file normalize "../verilog/slot_controller.sv"]"\
- "[file normalize "../verilog/spi_master.sv"]"\
- "[file normalize "../verilog/spi_slave.sv"]"\
- "[file normalize "../verilog/da_platform.sv"]"\
- "[file normalize "../verilog/da_platform_wrapper.sv"]"\
- "[file normalize "../verilog/structures.sv"]"\
- "[file normalize "../verilog/fifo_arbiter.sv"]"\
- "[file normalize "../verilog/fx2_model.sv"]"\
- "[file normalize "../verilog/i2s_receiver.sv"]"\
- "[file normalize "../verilog/i2s_source.sv"]"\
- "[file normalize "../verilog/interfaces.sv"]"\
- "[file normalize "../verilog/isolator_model.sv"]"\
- "[file normalize "../verilog/mig_adapter.sv"]"\
- "[file normalize "../verilog/slot_model.sv"]"\
- "[file normalize "../verilog/da_platform_tb.sv"]"\
-]
-add_files -norecurse -fileset $obj $files
-
-add_files -fileset $obj [file normalize "./da_platform_tb_behav.wcfg"]
-
-# Set 'sim_1' fileset file properties for remote files
-set_property -name "file_type" -value "SystemVerilog" -objects [get_files -of_objects [get_filesets sim_1] [list "*.sv"]]
-set_property -name "file_type" -value "Verilog Header" -objects [get_files -of_objects [get_filesets sim_1] [list "*.vh"]]
-
-# Set 'sim_1' fileset file properties for local files
-# None
-
-# Set 'sim_1' fileset properties
-set obj [get_filesets sim_1]
-set_property -name "nl.sdf_anno" -value "0" -objects $obj
-set_property -name "source_set" -value "" -objects $obj
-set_property -name "top" -value "da_platform_tb" -objects $obj
-set_property -name "verilog_define" -value "USE_WRAPPER USE_MIG_MODEL" -objects $obj
-set_property -name "xsim.simulate.runtime" -value "3000us" -objects $obj
+set_property -name "verilog_define" -value "USE_WRAPPER USE_MIG_MODEL" -objects [get_fileset sim_fake_mig]
+set_property -name "verilog_define" -value "USE_WRAPPER" -objects [get_fileset sim_real_mig]
 
 # Create 'serdes_tb' fileset (if not found)
 if {[string equal [get_filesets -quiet serdes_tb] ""]} {
