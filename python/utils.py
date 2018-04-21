f95c41dbbce375f9fca6d3405749893547b108d3
Michael Price <price@kilentra.net>
2018-04-21 17:47:01 -0400

    Clean up and rearrange Python scripts

--- python/utils.py
+++ python/utils.py
@@ -1,32 +1,6 @@
 
 from datetime import datetime
 
-#  This part seems to require FPGALink?
-"""
-
-import fl
-
-USB_VIDPID = "1443:0005"
-
-class FLDependentObject(object):
-    def __init__(self):
-        fl.flInitialise(0)
-
-        if ( not fl.flIsDeviceAvailable(USB_VIDPID) ):
-            print("Loading firmware...")
-            fl.flLoadStandardFirmware(USB_VIDPID, USB_VIDPID)
-
-            print("Awaiting...")
-            fl.flAwaitDevice(USB_VIDPID, 600)
-
-        self.handle = fl.flOpen(USB_VIDPID)
-        print 'Obtained FPGALink handle for USB device %s' % USB_VIDPID
-
-    def close(self):
-        fl.flClose(self.handle)
-"""
-
-
 def get_elapsed_time(time_start):
     time_end = datetime.now()
     time_diff = time_end - time_start
